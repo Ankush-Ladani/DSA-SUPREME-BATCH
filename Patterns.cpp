a57d9babb41b25196412c9e70bdc98ed7367b69b
Ankush Ladani <astarladani@gmail.com>
2023-03-10 10:14:50 +0530

    Patter Continued

--- Patterns.cpp
+++ Patterns.cpp
@@ -64,4 +64,110 @@ int main()
     // }
 
     //  NUMERIC HOLLOW FULL PYRAMID
+
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n - row - 1; col++)
+        {
+            cout << " ";
+        }
+        int start = 1;
+        for (int col = 0; col < 2 * row + 1; col++)
+        {
+            if (row == 0 || row == n - 1) //  try with or with row == n-1
+            {
+                if (col % 2 == 0)
+                {
+                    cout << start;
+                    start++;
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            else
+            {
+                if (col == 0)
+                {
+                    cout << 1;
+                }
+                else if (col == 2 * row + 1 - 1)
+                {
+                    cout << row + 1;
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    // for (int row = 0; row < n; row++)
+    // {
+    //     for (int col = 0; col < n - row - 1; col++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     int start = 1;
+    //     for (int col = 0; col < 2 * row + 1; col++)
+    //     {
+    //         if (col == 0 || col == 2 * row + 1 - 1)
+    //         {
+    //             if (col % 2 == 0)
+    //             {
+    //                 cout << start;
+    //                 start++;
+    //             }
+    //             else
+    //             {
+    //                 cout << " ";
+    //             }
+    //         }
+    //         else
+    //         {
+    //             cout << " ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int row = 0; row < n; row++)
+    // {
+    //     int k = 1;
+    //     for (int col = 0; col < n; col += 1)
+    //     {
+    //         if (col < n - row - 1)
+    //         {
+    //             cout << " ";
+    //         }
+    //         if (!(col < n - row - 1))
+    //         {
+    //             cout << " " << k++;
+    //         }
+    //         // if ((col - row) == (n - 1))
+    //         // {
+    //         //     cout << k;
+    //         // }
+    //         // k = k + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int row = 0; row < n; row++)
+    // {
+    //     int k = 1;
+    //     int y = 0;
+    //     for (int col = 0; col < n - row - 1; col++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int col = 0; col < row + 1; col++)
+    //     {
+    //         cout << k++ << " ";
+    //     }
+    //     cout << endl;
+    // }
 }
