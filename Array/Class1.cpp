c0a91f89c7a463819dc5d247674be8c43aa4517c
Ankush Ladani <astarladani@gmail.com>
2023-03-13 21:06:49 +0530

    Array Class 1

---
+++ Array/Class1.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // REVERSE AN ARRAY
+
+    // int arr[] = {1, 20, 3, 4, 8, -5, 7, 90};
+
+    // int start = 0;
+    // int end = 7;
+
+    // while (start <= end)
+    // {
+    //     swap(arr[start], arr[end]);
+    //     start++;
+    //     end--;
+    // }
+
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // PRINT EXTREME VALUES OF AN ARRAY
+
+    // int start = 0;
+    // int end = 7;
+
+    // while (start <= end)
+    // {
+    //     if (start == end)
+    //         cout << arr[start];
+    //     else
+    //         cout << arr[start] << " " << arr[end] << " ";
+    //     start++;
+    //     end--;
+    // }
+
+    // FIND MAXIMUM AND MINIMUM VALUE FROM AN ARRAY
+
+    // int maxi = INT16_MIN;
+    // int mini = INT16_MAX;
+
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     if (arr[i] > maxi)
+    //     {
+    //         maxi = arr[i];
+    //     }
+    //     if (arr[i] < mini)
+    //     {
+    //         mini = arr[i];
+    //     }
+    // }
+
+    // cout << "MAX " << maxi << endl;
+    // cout << "MIN " << mini << endl;
+
+    // COUNT 1'S AND 0'S IN AN ARRAY
+
+    // int arr[] = {0, 1, 0, 1, 1, 0, 1, 1, 1};
+
+    // int NumberOfZeroes = 0;
+    // int NumberOfOnes = 0;
+
+    // for (int i = 0; i < 9; i++)
+    // {
+    //     if (arr[i] == 0)
+    //     {
+    //         NumberOfZeroes++;
+    //     }
+    //     else
+    //     {
+    //         NumberOfOnes++;
+    //     }
+    // }
+
+    // cout << NumberOfOnes << "-1" << endl
+    //      << NumberOfZeroes << "-0" << endl;
+
+    // LINEAR SEARCH
+
+    // int arr[] = {1, 2, 3, 4, 5};
+
+    // int key;
+    // cin >> key;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if (arr[i] == key)
+    //     {
+    //         cout << "Found...";
+    //         break;
+    //     }
+    // }
+
+    // PRINT DOUBLES OF VALUES OF AN ARRAY
+
+    // int n;
+    // cin >> n;
+
+    // int arr[n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+
+    // cout << "DOUBLE OF ENTERED THESE VALUES:- ";
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << 2 * arr[i] << " ";
+    // }
+}
