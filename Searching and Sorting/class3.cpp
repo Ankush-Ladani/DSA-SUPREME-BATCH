5111b3d5c2b9e467d63af8c8c206146b8949a40b
Ankush Ladani <astarladani@gmail.com>
2023-03-29 09:33:53 +0530

    Done with 2 Questions searchInRotatedAndSorted Array and findPivot

--- Searching and Sorting/class3.cpp
+++ Searching and Sorting/class3.cpp
@@ -43,11 +43,81 @@ int findOdd(vector<int> arr)
     return mid;
 }
 
+int findPivot(vector<int> arr)
+{
+    int s = 0;
+    int e = arr.size() - 1;
+
+    int mid = s + (e - s) / 2;
+
+    while (s <= e)
+    {
+        if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+
+bool binarySearch(vector<int> arr, int start, int end, int key)
+{
+    int mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return 1;
+        }
+        else if (arr[mid] > key)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+
+bool searchInRotatedAndSorted(vector<int> arr, int key, int pivot)
+{
+    if (key >= arr[pivot] && key <= arr[arr.size() - 1])
+    {
+        return binarySearch(arr, pivot, arr.size() - 1, key);
+    }
+    else
+    {
+        return binarySearch(arr, 0, pivot - 1, key);
+    }
+    return false;
+}
+
 int main()
 {
-    vector<int> arr{1, 2, 2, 3, 3};
 
-    int ans = findOdd(arr);
+    vector<int> arr{8, 10, 17, 1};
 
-    cout << ans;
+    int k;
+    cin >> k;
+
+    int ans = findPivot(arr);
+
+    bool ans2 = searchInRotatedAndSorted(arr, k, ans);
+
+    cout << ans2 << endl;
+
+    // vector<int> arr{1, 2, 2, 3, 3};
+
+    // int ans = findOdd(arr);
+
+    // cout << ans;
 }
